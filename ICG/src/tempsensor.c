7b6beae73703d47ef885e517b49a1936a674206a
ahmed7ramdan <ahmedramdan.st@gmail.com>
2023-09-18 20:41:33 +0300

    Implement the Function Under Test

--- ICG/src/tempsensor.c
+++ ICG/src/tempsensor.c
@@ -1,6 +1,8 @@
 #include "tempsensor.h"
-
+#include "i2c.h"
+#include <stdint.h>
 float getTemperature(void)
 { 
-    return 0.0f;
+    uint16_t rawValue = i2c_readRegister(0x03);
+    return -100.0f + (0.2f * (float)rawValue);  
 }
